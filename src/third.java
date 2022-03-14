1d60c11b36ba2d0be4737723a6ffb7be50ac0983
Vlad Romanovich <3301889v@gmail.com>
2022-03-14 20:15:34 +0300

    f

---
+++ src/third.java
@@ -0,0 +1,27 @@
+public class third {
+    // первое
+
+    private static Boolean n(int b) {
+        b = 357;
+        return b % 10 == 7;
+    }
+
+    // Второе
+
+
+    public static Boolean n() {
+        int a = 2;
+        int b = 4;
+        int r = 2;
+        if ((r * 2) > a && (r * 2) > b) {
+            return true;
+
+        } else {
+            return false;
+
+        }
+
+    }
+
+
+}
