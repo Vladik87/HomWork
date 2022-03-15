e47a3677881d7a4497b5625b7659c8222d3b6668
Vlad Romanovich <3301889v@gmail.com>
2022-03-15 20:31:39 +0300

    Добавленно еще одно условие для задания 5

--- src/third_5.java
+++ src/third_5.java
@@ -1,15 +1,15 @@
 public class third_5 {
     public static void main(String[] args) {
-        int a=1;
-        int b=1;
-        int c=1;
-        int d=2;
-        int e=2;
-        int f=2;
-        if (((e - a >= c) && (f >= b) && (b >= d)) || ((e - a >= d) && ((f >= b) && (b >= c))) || ((e - b >= d) && (f - a >= c)) || ((e - a >= c) && (f - b >= d)) || ((e - a >= d) && (f - b >= c))) {
+        int a = 6;
+        int b = 1;
+        int c = 1;
+        int d = 2;
+        int e = 8;
+        int f = 2;
+        if (((e - a >= c) && (f >= b) && (b >= d)) || ((e - a >= d) && ((f >= b) && (b >= c))) || ((e - b >= d) && (f - a >= c)) || ((e - a >= c) && (f - b >= d)) || ((e - a >= d) && (f - b >= c)) || ((e - b >= d) && (f - a >= c))) {
             System.out.println("Дома входят границы участка");
-        }else {
+        } else {
             System.out.println("Дома не входят");
-    }
+        }
     }
 }
